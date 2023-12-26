7fce8b6c1aa4b912407c1ef71d80b6960178928b
Kishor978 <thaunnakishor@gmail.com>
2023-12-26 21:47:33 +0545

    finding center position

--- main.py
+++ main.py
@@ -8,7 +8,39 @@ from pygame.locals import *
 HEIGHT = 650
 WIDTH = 650
 
+INORGANIC_TARGET_IDS = {16, 17, 18, 19, 20}
+ORGANIC_TARGET_IDS = {11, 12, 13, 14, 15}
+DESTINATION_IDS = {8, 9}
+BOT_IDS = {5, 6}
+BOUNDARY_MARKER_IDS = {0, 2, 3, 1}
+
+def initialize_pygame():
+    """
+    Inintializing pygame 
+    Return: 
+        pygame screen
+    """
+    pygame.init()
+    marker_screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Marker Positions")
+    return marker_screen
+
+
+
 def aruco_detector(img,dictionaries,camera_matrix,distortion_coefficients,draw=True):
+    """
+    this function detect the aruco markers 
+    
+    Agrs:
+    img: video frame
+        dictionaries: aruco dictionary used
+        camera_matrix: camera matrix from camera calibration
+        distortion_cofficient: distortion matrix form camera calibration
+
+    return:
+        corners: a list of corners of arucos
+        ids: Ids of aruco markers
+    """
     dictionary=aruco.Dictionary_get(dictionaries)
     corners, ids, _ = aruco.detectMarkers(img, dictionary)
     # camera calibration
@@ -20,6 +52,49 @@ def aruco_detector(img,dictionaries,camera_matrix,distortion_coefficients,draw=T
 
     return corners,ids
 
+def find_position(frame,ids,corners,marker_screen,marker_centers):  
+    """"""  
+    # running = True
+    # while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    # pygame_camera_frame = pygame.surfarray.make_surface(camera_frame.swapaxes(0, 1))
+    pygame.display.flip()
+    # Draw marker positions on the marker window
+    marker_screen.fill((255, 255, 255))  # Clear the marker window
+    if ids is not None:
+        for i in range(len(ids)):
+            marker_pos = (int(corners[i][0, 0, 0]), int(corners[i][0, 0, 1]))
+            pygame.draw.circle(marker_screen, (255, 0, 0), marker_pos, 5)  # Draw a red circle at the marker position
+        if len(marker_centers) == len(BOUNDARY_MARKER_IDS):
+            # Create a convex hull from the marker centers
+            hull = cv.convexHull(np.array(marker_centers))
+
+            # Draw the convex hull on the camera feed
+            cv.polylines(frame, [hull], isClosed=True, color=(0, 255, 0), thickness=2)
+
+            # Create a mask for the area inside the convex hull
+            mask = np.zeros_like(frame)
+            cv.fillPoly(mask, [hull], (255, 255, 255))
+            frame_inside_boundary = cv.bitwise_and(frame, mask)
+
+            # Resize the mapped environment frame to match map window size
+            frame_inside_boundary = cv.resize(frame_inside_boundary, (WIDTH, HEIGHT))
+
+            # Convert the mapped environment frame to Pygame surface
+            frame_inside_boundary = cv.cvtColor(frame_inside_boundary, cv.COLOR_BGR2RGB)
+            pygame_map_frame = pygame.surfarray.make_surface(frame_inside_boundary.swapaxes(0, 1))
+
+            # Blit the mapped environment frame onto the map window
+            marker_screen.blit(pygame_map_frame, (0, 0))
+
+            # Update the map display
+            pygame.display.flip()
+    # Update the marker display
+    pygame.display.flip()
+pygame.quit()
 
 def main():
     cap=cv.VideoCapture(0)
