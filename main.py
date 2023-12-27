adb6b8a958f8cb549ccb4baa86bdd73198497aa7
Kishor978 <thaunnakishor@gmail.com>
2023-12-27 10:28:59 +0545

    finding distance between 2 arucos

--- main.py
+++ main.py
@@ -52,8 +52,14 @@ def aruco_detector(img,dictionaries,camera_matrix,distortion_coefficients,draw=T
 
     return corners,ids
 
-def find_position(frame,ids,corners,marker_screen,marker_centers):  
-    """"""  
+def mapping_boundary(frame,ids,corners,marker_screen,boundary_marker_centers):  
+    """This function draws maps in pygame.
+    Args:
+    marker_screen: pygame frame
+    boundary_marker_center: centre of boundary aruco markers
+
+    Returns: none
+    """  
     # running = True
     # while running:
     for event in pygame.event.get():
@@ -68,9 +74,9 @@ def find_position(frame,ids,corners,marker_screen,marker_centers):
         for i in range(len(ids)):
             marker_pos = (int(corners[i][0, 0, 0]), int(corners[i][0, 0, 1]))
             pygame.draw.circle(marker_screen, (255, 0, 0), marker_pos, 5)  # Draw a red circle at the marker position
-        if len(marker_centers) == len(BOUNDARY_MARKER_IDS):
+        if len(boundary_marker_centers) == len(BOUNDARY_MARKER_IDS):
             # Create a convex hull from the marker centers
-            hull = cv.convexHull(np.array(marker_centers))
+            hull = cv.convexHull(np.array(boundary_marker_centers))
 
             # Draw the convex hull on the camera feed
             cv.polylines(frame, [hull], isClosed=True, color=(0, 255, 0), thickness=2)
@@ -96,12 +102,34 @@ def find_position(frame,ids,corners,marker_screen,marker_centers):
     pygame.display.flip()
 pygame.quit()
 
+
+def aruco_center_position(ids,corners,given_marker_ids):
+    """this function finds the center of the aruco marker
+    args:
+        given_marker_ids: list of aruco markers id
+
+    Returns: center of the aruco markers
+    """
+    center_position=[]
+    output=[]
+    if ids is not None:
+        for i in range(len(ids)):
+            if ids[i][0] in given_marker_ids:
+                marker_center = np.mean(corners[i][0], axis=0, dtype=np.int32)
+                center_position.append(marker_center)
+                # output = [list(arr) for arr in center_position]
+    return center_position
+
+
 def main():
     cap=cv.VideoCapture(0)
     dictionaries = cv.aruco.DICT_6X6_250
                 # cv.aruco.DICT_6X6_1000,
                 # cv.aruco.DICT_6X6_50,
                 # cv.aruco.DICT_6X6_100,
+    # initializing pygame
+    marker_screen = initialize_pygame()
+    #  load calibration data
     calib_data_path = "E:\\swarmnoid\\calib_data\\MultiMatrix.npz"
     calib_data = np.load(calib_data_path)
 
@@ -120,6 +148,16 @@ def main():
         # detecting markers and getting cornors
         corners,id=aruco_detector(img_gray,dictionaries,cam_mat,dist_coef,draw=True)
         
+        # finding the centers of the aruco markerss
+        boundary_markers_center=aruco_center_position(id,corners,BOUNDARY_MARKER_IDS)
+        organic_marker_center=aruco_center_position(id,corners,ORGANIC_TARGET_IDS)
+        inorganic_marker_center=aruco_center_position(id,corners,INORGANIC_TARGET_IDS)
+        destination_marker_center=aruco_center_position(id,corners,DESTINATION_IDS)
+        bot_marker_center=aruco_center_position(id,corners,BOT_IDS)
+
+        # mapping boundary and drawing convex hall
+        mapping_boundary(img_gray,id,corners,marker_screen,boundary_markers_center)
+
         cv.imshow("Frame",img_gray)
         if cv.waitKey(1) & 0xFF == ord("q"):
             break
